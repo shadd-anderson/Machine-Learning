53298077f1ac2e12e7e8f69568c1d5d26cacc948
Shadd Anderson <shaddanderson18@gmail.com>
2018-07-11 02:02:22 -0500

    Section 5.42 (Predicting test set)

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -27,3 +27,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Fitting Multiple Linear Regression to the training set
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = regressor.predict(X_test)
