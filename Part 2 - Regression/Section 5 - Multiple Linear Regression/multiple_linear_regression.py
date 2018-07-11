8e2aceac38baffa0942570f349b4ca52d159e71f
Shadd Anderson <shaddanderson18@gmail.com>
2018-07-11 01:40:34 -0500

    Section 5.40 (Preparing the data)

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Importing data set and setting independent/dependent variables
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1:].values
+
+# Encoding categorical data
+# --Encodes country category from string to integer
+labelendcoder_X = LabelEncoder()
+X[:, 3] = labelendcoder_X.fit_transform(X[:, 3])
+# --Transforms country integers to 3 "true or false" columns for each country
+# --This is because there is no hierarchy to the countries, they're simply categories
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting dataset into Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
