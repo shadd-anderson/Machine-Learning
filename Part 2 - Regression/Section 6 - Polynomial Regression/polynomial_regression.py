6a60164ab86c5785074b550dc814f348f5023e08
Shadd Anderson <shaddanderson18@gmail.com>
2018-07-14 02:47:25 -0500

    Fixing plots

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -1,9 +1,11 @@
+import matplotlib
 import matplotlib.pyplot as plot
 import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+matplotlib.use('TkAGG')
 
 # Importing data set and setting independent/dependent variables
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -32,7 +34,7 @@ plot.ylabel("Salary")
 plot.show()
 
 # Visualizing the polynomial regression result
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.05)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plot.scatter(X, y, color="red")
 plot.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color="blue")
