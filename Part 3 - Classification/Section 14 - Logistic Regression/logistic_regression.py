7451ea65c974895ee8393cd700e7f5f3d3c691af
Shadd Anderson <shaddanderson18@gmail.com>
2018-07-16 22:03:36 -0500

    Section 12.85 - Pre-processing the data

---
+++ Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plot
+import pandas as pd
+
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Importing data set and setting independent/dependent variables
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1:].values
+
+# Splitting dataset into Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Feature scaling (to ensure there are no funky issues with data processing)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
